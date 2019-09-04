69c520fe745be1df04931c2515c085a1612815de
nazarenogabrielbastanzo <b.nazarenogabriel@gmail.com>
2019-09-04 12:31:05 -0300

    Update dispositivos_view.php

--- application/views/dispositivos_view.php
+++ application/views/dispositivos_view.php
@@ -7,7 +7,7 @@ $this->load->view('menu_view');
 <section class="container mt-3">
 <h4><?php echo $current; ?></h4>
     <div style="text-align: center;">
-            <p style="text-align: center; margin-top: 5px;">Usando un lector de códigos QR en el teléfono celular o en una tablet, puedes apuntar con la cámara del dispositivo la siguiente imagen, para cargar esta página en su navegador.</p>
+            <p style="text-align: center; margin-top: 5px;">Usando un lector de códigos QR en el teléfono celular o en una tablet, puedes apuntar con la cámara del dispositivo la siguiente imagen, para cargar este sitio en el navegador de su dispositivo.</p>
             <img src="<?= base_url('assets/img/exported_qrcode_image.png'); ?>" alt="Código QR" class="img-fluid"/>
         </div>
     </div>
