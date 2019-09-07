79d4302d11d6bba87c35276420e90c431c022732
nazarenogabrielbastanzo <b.nazarenogabriel@gmail.com>
2019-09-06 22:26:07 -0300

    Update torneos_view.php

--- application/views/visor/torneos_view.php
+++ application/views/visor/torneos_view.php
@@ -16,66 +16,6 @@ $this->load->view('menu_view');
       </div>
     </div>
   </div>
-  <div class="my-5">
-    <div class="card shadow">
-      <div class="card-header">
-        <h4>GCT Saint Louis Rapid 2019</h4>
-      </div>
-      <div class="card-body">
-        <div style="width:900px; height: 600px; margin: auto;">
-          <iframe src="https://live.chessbase.com/pgn/gct-saint-louis-rapid-2019/0" width="900" height="600"></iframe>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="my-5">
-    <div class="card shadow">
-      <div class="card-header">
-        <h4>Levitov Chess Week Rapid 2019</h4>
-      </div>
-      <div class="card-body">
-        <div style="width:900px; height: 600px; margin: auto;">
-          <iframe src="https://live.chessbase.com/pgn/levitov-chess-week-rapid-2019/0" width="900" height="600"></iframe>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="my-5">
-    <div class="card shadow">
-      <div class="card-header">
-        <h4>Grand Prix Riga 2019</h4>
-      </div>
-      <div class="card-body">
-        <div style="width:900px; height: 600px; margin: auto;">
-          <iframe src="https://live.chessbase.com/pgn/grand-prix-riga-2019/0" width="900" height="600"></iframe>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="my-5">
-    <div class="card shadow">
-      <div class="card-header">
-        <h4>7th Altibox Norway Chess 2019</h4>
-      </div>
-      <div class="card-body">
-        <div style="width:900px; height: 600px; margin: auto;">
-          <iframe src="https://live.chessbase.com/pgn/7th-altibox-norway-chess-2019/0" width="900" height="600"></iframe>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="my-5">
-    <div class="card shadow">
-      <div class="card-header">
-        <h4>XIV American Continental-ch 2019</h4>
-      </div>
-      <div class="card-body">
-        <div style="width:900px; height: 600px; margin: auto;">
-          <iframe src="https://live.chessbase.com/pgn/xiv-american-continental-ch-2019/0" width="900" height="600"></iframe>
-        </div>
-      </div>
-    </div>
-  </div>
 </div>
 
 <?php $this->load->view('footer_view'); ?>
