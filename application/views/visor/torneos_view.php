e17332baa47e1a579022732fede185884315cdc7
nazarenogabrielbastanzo <b.nazarenogabriel@gmail.com>
2019-09-10 20:37:00 -0300

    Update torneos_view.php, 160.png, and 3 more files...

--- application/views/visor/torneos_view.php
+++ application/views/visor/torneos_view.php
@@ -7,11 +7,11 @@ $this->load->view('menu_view');
   <div class="my-5">
     <div class="card shadow">
       <div class="card-header">
-        <h4>7th Sinquefield Cup GCT 2019</h4>
+        <h4>FIDE World Cup 2019</h4>
       </div>
       <div class="card-body">
         <div style="width:900px; height: 600px; margin: auto;">
-          <iframe src="https://live.chessbase.com/pgn/7th-sinquefield-cup-gct-2019/0" width="900" height="600"></iframe>
+          <iframe src="https://live.chessbase.com/pgn/fide-world-cup-2019/0" width="900" height="600"></iframe>
         </div>
       </div>
     </div>
